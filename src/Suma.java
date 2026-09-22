34d0e8874ce7fe50fece652b00c1d620e75dac0b
iker-tixicuro <adam24itixicuro@inslaferreria.cat>
2026-09-22 17:02:32 +0200

    feat: float y double

--- src/Suma.java
+++ src/Suma.java
@@ -1,6 +1,14 @@
 class Suma{
 
       public static  int suma (int a, int b){
-       return a+b;
+        return a+b;
+      }
+
+      public static float suma(float a, float b){
+        return a+b;
+      }
+
+      public static double suma (double a, double b){
+        return a+b;
       }
 }
